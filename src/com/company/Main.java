f392c363f39a4d737dc4ebe794b908141e8dce00
hexma999 <hexma999@naver.com>
2018-03-27 00:52:34 +0900

    add .gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Hellow World");
         System.out.println("Project Diva");
 
-        System.out.println("Project Roct2");
+        System.out.println("Project Roct1");
     }
 }
